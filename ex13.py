69e3ee64bfacfee7ae4e3301361ac9316dfc1847
cc <shen_nanping@163.com>
2021-09-04 22:05:00 +0800

    feat(ex13): update ex13.py

--- ex13.py
+++ ex13.py
@@ -6,3 +6,9 @@ print("the script is called:", script)
 print("your first variable is:", first)
 print("your second variable is:", second)
 print("your third variable is:", third)
+
+# e.g. python ex13.py a b c
+# the one after python command is script and after it are arguments
+
+
+# the difference between 'input' & 'argv' is that params needs to be entered is argv, params needs to be entered when you need it is input
